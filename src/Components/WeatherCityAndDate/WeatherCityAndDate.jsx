6304e11a20766d8033a560bba7e1638fc3227198
FedorLivshits <fediliv@gmail.com>
2021-04-06 15:45:17 +0300

    added real date

--- src/Components/WeatherCityAndDate/WeatherCityAndDate.jsx
+++ src/Components/WeatherCityAndDate/WeatherCityAndDate.jsx
@@ -6,10 +6,15 @@ function WeatherCityAndDate({city, country}) {
         <div className="city__and__date">
             <div className="city-name">{city}, {country}</div>
             <div className="date">
-                {new Date().toLocaleString('en', {weekday: 'long'})} {new Date().getDay()} April
+                {new Date().toLocaleString('en', {weekday: 'long'})}
+                {" "}
+                {new Date().getDate()}
+                {" "}
+                {new Date().toLocaleString('en', {month: 'long'})}
             </div>
         </div>
     )
 
 }
+
 export default WeatherCityAndDate
